995aa89fb59551b8bcb62303c00a09592655440b
Aaron Shaver <AaronShaverPDX@gmail.com>
2019-04-29 16:10:21 -0700

    Inline division; user input

--- 003.hs
+++ 003.hs
@@ -1,5 +1,3 @@
-numberUnderTest = 50113
-
 isPrime :: Int -> Bool
 isPrime x
     | x < 2 = False
@@ -8,18 +6,16 @@ isPrime x
     let denominatorsList = [2..x-1]
     in filter (==0) (map (x `rem`) denominatorsList) == []
 
-dividesEvenly :: Int -> Bool
-dividesEvenly denominator = do
-    numberUnderTest `rem` denominator == 0
-
 largestPrimeFactor :: Int -> Int
 largestPrimeFactor x = do
     let checkForPrimality = [x,x-1..2]
-    let primesList = filter (isPrime) checkForPrimality
-    let isFactorList = filter (dividesEvenly) primesList
-    head isFactorList
+    let primesList = filter (isPrime ) checkForPrimality
+    head (filter (\n -> x `rem` n == 0) primesList)
 
 main :: IO ()
 main = do
+    putStrLn "enter a number to find highest prime factor: "
+    input1 <- getLine
+    let numberUnderTest = read input1 :: Int
     let result = largestPrimeFactor numberUnderTest 
     print result
