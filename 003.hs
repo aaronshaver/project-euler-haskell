f63dca4bbf64edd80a696dc92f2412104f6ed78b
Aaron Shaver <AaronShaverPDX@gmail.com>
2019-04-29 12:18:00 -0700

    003 (largest prime factor) solved, but it's slow

--- 003.hs
+++ 003.hs
@@ -1,6 +1,25 @@
-generatePrimes x = x : generatePrimes (x + 1)
+numberUnderTest = 50113
+
+isPrime :: Int -> Bool
+isPrime x
+    | x < 2 = False
+    | x == 2 = True
+    | otherwise =
+    let denominatorsList = [2..x-1]
+    in filter (==0) (map (x `rem`) denominatorsList) == []
+
+dividesEvenly :: Int -> Bool
+dividesEvenly denominator = do
+    numberUnderTest `rem` denominator == 0
+
+largestPrimeFactor :: Int -> Int
+largestPrimeFactor x = do
+    let checkForPrimality = [x,x-1..2]
+    let primesList = filter (isPrime) checkForPrimality
+    let isFactorList = filter (dividesEvenly) primesList
+    head isFactorList
 
 main :: IO ()
 main = do
-    let result = takeWhile (<= 50) (generatePrimes 1)
+    let result = largestPrimeFactor numberUnderTest 
     print result
