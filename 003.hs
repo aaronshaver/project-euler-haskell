bc5175b72e1ea7a6587817fc4e5dfb591d08a7eb
Aaron Shaver <AaronShaverPDX@gmail.com>
2019-05-02 07:32:57 -0700

    Attempted to optimize isPrime; didn't work

--- 003.hs
+++ 003.hs
@@ -1,21 +1,16 @@
 isPrime :: Int -> Bool
 isPrime inputNumber
-    | inputNumber < 2 = False
-    | inputNumber == 2 = True
+    | elem (mod inputNumber 10) [2, 4, 5, 6, 8, 0] == True = False
     | otherwise =
     let denominatorsList = [2..inputNumber-1]
-    -- divide input by each denominator from 2 to N-1
-    -- then filter that list for any zeros (lack of remainders)
-    -- then return a bool of whether that filtered list is empty
     in filter (==0) (map (inputNumber `rem`) denominatorsList) == []
 
 largestPrimeFactor :: Int -> Int
 largestPrimeFactor inputNumber = do
-    -- note: will output the input number itself if it's prime!
-    head (filter (\n -> inputNumber `rem` n == 0 && isPrime n == True) [inputNumber,inputNumber-1..2])
+    head (filter (\n -> inputNumber `rem` n == 0 && isPrime n == True) [inputNumber, inputNumber-1..2])
 
 main :: IO ()
 main = do
     -- HINT: do :set +s in ghci to enable timing and memory usage
-    let result = largestPrimeFactor 50011
+    let result = largestPrimeFactor 18000001
     print result
