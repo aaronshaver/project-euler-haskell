150903dbfff2816314d80ec539809c0cd5ecc83f
Aaron Shaver <AaronShaverPDX@gmail.com>
2019-05-02 19:40:43 -0700

    Code style improvement

--- 003.hs
+++ 003.hs
@@ -1,14 +1,14 @@
 isPrime :: Int -> Bool
 isPrime inputNumber
-    -- ignoring special cases of 2, 5 for this problem
-    | elem (mod inputNumber 10) [2, 4, 5, 6, 8, 0] == True = False
-    | otherwise =
-    let denominatorsList = [2..inputNumber-1]
-    in filter (==0) (map (inputNumber `rem`) denominatorsList) == []
+    | elem (mod inputNumber 10) [2, 4, 5, 6, 8, 0] == True = False -- ignoring special cases of 2, 5 for this problem
+    -- null is a function that takes a list and tells you whether or not the list is empty
+    | otherwise = null $ filter (==0) (map (inputNumber `rem`) denominatorsList)
+    where denominatorsList = [2..inputNumber-1]
 
 largestPrimeFactor :: Int -> Int
-largestPrimeFactor inputNumber = do
-    head (filter (\n -> inputNumber `rem` n == 0 && isPrime n == True) [inputNumber, inputNumber-1..2])
+largestPrimeFactor inputNumber =
+    head $ filter isPrimeFactor [inputNumber, inputNumber-1..2]
+    where isPrimeFactor n = inputNumber `rem` n == 0 && isPrime n
 
 main :: IO ()
 main = do
