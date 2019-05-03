a3c595c1caf86a041c9f9c9654dbeb67f415714d
Aaron Shaver <AaronShaverPDX@gmail.com>
2019-05-02 19:50:01 -0700

    sqrt of input for isPrime but makes hardly any diff

--- 003.hs
+++ 003.hs
@@ -3,15 +3,19 @@ isPrime inputNumber
     | elem (mod inputNumber 10) [2, 4, 5, 6, 8, 0] == True = False -- ignoring special cases of 2, 5 for this problem
     -- null is a function that takes a list and tells you whether or not the list is empty
     | otherwise = null $ filter (==0) (map (inputNumber `rem`) denominatorsList)
-    where denominatorsList = [2..inputNumber-1]
+    where
+        highestDenominator = round (sqrt (fromIntegral inputNumber))
+        denominatorsList = [2..highestDenominator]
 
 largestPrimeFactor :: Int -> Int
 largestPrimeFactor inputNumber =
     head $ filter isPrimeFactor [inputNumber, inputNumber-1..2]
-    where isPrimeFactor n = inputNumber `rem` n == 0 && isPrime n
+    where
+        isPrimeFactor :: Int -> Bool
+        isPrimeFactor n = inputNumber `rem` n == 0 && isPrime n
 
 main :: IO ()
 main = do
     -- HINT: do :set +s in ghci to enable timing and memory usage
-    let result = largestPrimeFactor 18000001
+    let result = largestPrimeFactor 19999991
     print result
